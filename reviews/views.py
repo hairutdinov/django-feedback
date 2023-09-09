af8f4245753c28298be2db77bf6e8c5ddcacea0a
bulat <bulatxajrutdinoff@yandex.ru>
2023-09-09 15:54:09 +0300

    1. creating thank-you view and template 2. creating django form 3. rendering django form in template

--- reviews/views.py
+++ reviews/views.py
@@ -1,7 +1,21 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-
-# Create your views here.
-
+from django.urls import reverse
+from .forms import ReviewForm
 
 def review(request):
-    return render(request, 'reviews/review.html')
+    form = ReviewForm()
+
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            return HttpResponseRedirect(reverse('thank-you'))
+
+    return render(request, 'reviews/review.html', {
+        "form": form
+    })
+
+
+def thank_you(request):
+    return render(request, 'reviews/thank_you.html')
