5c49b8ceb0dd22e3966d2b5b9c71bec99674dce5
bulat <bulatxajrutdinoff@yandex.ru>
2023-09-10 08:54:38 +0300

    using CreateView instead a View

--- profiles/views.py
+++ profiles/views.py
@@ -1,27 +1,13 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
+from django.views.generic.edit import CreateView
 from .forms import ProfileForm
 from .models import UserProfile
 
 
-class CreateProfileView(View):
-    def get(self, request):
-        form = ProfileForm()
-        return render(request, "profiles/create_profile.html", {
-            "form": form
-        })
-
-    def post(self, request):
-        form = ProfileForm(request.POST, request.FILES)
-
-        if form.is_valid():
-            profile = UserProfile(image=request.FILES['user_image'])
-            profile.save()
-            return HttpResponseRedirect('/profiles')
-
-        return render(request, "profiles/create_profile.html", {
-            "form": form
-        })
-
-
+class CreateProfileView(CreateView):
+    model = UserProfile
+    fields = '__all__'
+    template_name = 'profiles/create_profile.html'
+    success_url = '/profiles'
