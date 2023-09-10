e07fb6f6c5231c1404813faab546d19cd481b07c
bulat <bulatxajrutdinoff@yandex.ru>
2023-09-10 09:20:43 +0300

    1. user profiles view 2. serving uploaded files

--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
 from django.views.generic.edit import CreateView
+from django.views.generic import ListView
 from .forms import ProfileForm
 from .models import UserProfile
 
@@ -11,3 +12,9 @@ class CreateProfileView(CreateView):
     fields = '__all__'
     template_name = 'profiles/create_profile.html'
     success_url = '/profiles'
+
+
+class UserProfilesView(ListView):
+    template_name = 'profiles/user_profiles.html'
+    model = UserProfile
+    context_object_name = 'profiles'
