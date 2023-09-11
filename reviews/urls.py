b9ccfc292ab4cc15db14422af5f02f15957aebb0
bulat <bulatxajrutdinoff@yandex.ru>
2023-09-11 08:53:25 +0300

    1. sessions 2. adding Favorite button

--- reviews/urls.py
+++ reviews/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.ReviewView.as_view(), name='review'),
     path('thank-you', views.ThankYouView.as_view(), name='thank-you'),
     path('reviews', views.ReviewsListView.as_view(), name='reviews-list'),
-    path('reviews/<int:pk>', views.ReviewDetailView.as_view(), name='review-detail')
+    path('reviews/favorite', views.AddFavoriteView.as_view()),
+    path('reviews/<int:pk>', views.ReviewDetailView.as_view(), name='review-detail'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -35,3 +35,17 @@ class ReviewDetailView(DetailView):
     template_name = 'reviews/review-detail.html'
     model = Review
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        loaded_review = self.object
+        request = self.request
+        favorite_id = request.session.get('favorite_review')
+        context['is_favorite'] = favorite_id == str(loaded_review.id)
+        return context
+
+
+class AddFavoriteView(View):
+    def post(self, request):
+        review_id = request.POST['review_id']
+        request.session['favorite_review'] = review_id
+        return HttpResponseRedirect(reverse('review-detail', args=[review_id]))
